590f4b15047d9f9d9bd5f9cbde08192d86f2c760
smartbyter <smartbyter@yandex.ru>
2026-06-19 15:44:08 +1000

    Update run-server.sh

--- run-server.sh
+++ run-server.sh
@@ -3,7 +3,8 @@
 # run from ed-ipc2030
 
 # ssh pi@192.168.10.130
-export PATH=$PATH:/home/pi/deno
+PATH_PROJECT=/home/pi/joystick-can-bus
+export PATH=$PATH:$PATH_PROJECT
 
 sudo ip link set canb0 down && sudo ip link set canb0 type can bitrate 1000000 && sudo ip link set canb0 up
 
@@ -11,4 +12,4 @@ deno run \
     --allow-net \
     --allow-run \
     --allow-read \
-    /home/pi/deno/can2ws.ts
+    $PATH_PROJECT/can2ws.ts
