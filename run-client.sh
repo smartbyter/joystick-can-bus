e8fba50b007c49b69aa315c038f045d6636c859d
smartbyter <smartbyter@yandex.ru>
2026-06-19 15:42:14 +1000

    Update run-client.sh

--- run-client.sh
+++ run-client.sh
@@ -12,6 +12,6 @@ deno run \
     --allow-ffi \
     --unstable-ffi \
     --allow-read \
-    /data/projects/deno/joystick/ws2uinput.ts \
+    ~/projects/joystick-can-bus/ws2uinput.ts \
     --ip 192.168.10.130 \
     --port 8080
